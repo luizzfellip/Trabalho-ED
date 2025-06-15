cee17e9070ba27c81996b92952e01435fcfa40d2
luizzfellip <luizfresende26@gmail.com>
2025-06-15 20:15:31 -0300

    Add files via upload

--- Merge Multiway.cpp
+++ Merge Multiway.cpp
@@ -2,9 +2,7 @@
 // 1- modifiquei o tamanho dos registros e a parte da distruibuicao dos arquivos
 // para que aceite o int e o float.
 
-// 2- nao terminei, mas falta implementar o merge sort interno para depois depois
-// fazer o merge multi way.
-
+// 2- Implementei o merge sort interno,falta implementar depois o merge multi way.
 
 #include <iostream>
 #include <fstream>
@@ -15,9 +13,8 @@
 using namespace std;
 
 	// Definicao do numero de arquivos divididos  e nomes dos arquivos
-	const int numPartes = 12;
+	const int numPartes = 13;
 	const string nomeBaseAqr = "parte_F";
-	const string nomeBaseArqOrdenada = "parte_ordenada_F";
 struct Registro
 {
 		char series_reference[10];
@@ -131,28 +128,93 @@ void dividir_CSV_em_PartesBinarias()
     // 6. liberar a memória alocada para o array de arquivos
     delete[] arqSaidaBin;
     
-    cout << "Arquivo dividido com sucesso!\n";
+    cout << "Arquivo dividido em binario com sucesso!\n";
 
 }
 
-void ordenar_partes_internamente(){
+long long tamanho_Registro(const string& nomeArquivo)
+{
+    ifstream arquivo(nomeArquivo, ios::binary | ios::ate); // vai ate o final do arquivo
+    if (!arquivo.is_open()){
+		throw runtime_error("Nao foi possivel abrir o arquivo!");
+	}
+    long long tamanho = arquivo.tellg(); // diz o tamanho
+    return (tamanho / sizeof(Registro));
+}
+
+void intercala(Registro v[], long p, long q, long r)
+{
+    long i = p, j = q;
+    long tamanho = r - p + 1;
+    Registro* aux = new Registro[tamanho]; // vetor auxiliar
+    long k = 0;
+    while((i < q) and (j <= r)){                
+		// verifica se v[i] - v[j] > 0, se sim v[i] > v[j]
+       if(strcmp(v[i].series_reference,v[j].series_reference) > 0){
+           aux[k++] = v[i++];
+		}else{ 
+			aux[k++] = v[j++];
+		}
+	} 
+// terminou um dos vetores, agora copia o outro
+	while (i < q) {
+   		aux[k++] = v[i++];
+	}   
+    while (j < r) {
+       aux[k++] = v[j++]; 
+    }
+    // agora copiamos do vetor auxiliar aux[] em v[p:r]
+	for (long m = 0; m < tamanho; m++){ 
+		v[p + m] = aux[m];
+    }
+	delete [] aux;
+}
+
+void mergeSortIterativo(Registro v[], long tam)
+{
+    long p, r, b = 1;
+    while (b < tam) {
+        p = 0;
+        while (p + b < tam) {
+            r = p + 2*b - 1;
+            if (r >= tam) r = tam - 1;
+            intercala(v, p, p+b-1, r);
+            p = p + 2*b;
+        }
+        b = 2*b; // dobra o tamanho do bloco
+	}
+}
+
+void ordenar_partes_internamente()
+{
 	cout << "Iniciando a ordenacao interna dos arquivos!\n";
 
-	// 1. criar as partes divididas
-	ofstream* arqSaidaBinOrdenada = new ofstream[numPartes];
+	// 1. alocar na memoria um array com os mesmos numeros de registros
 	for(int i=0; i<numPartes; i++){
-		string nomeArq = nomeBaseArqOrdenada +  to_string(i) + ".bin";
-		arqSaidaBin[i].open(nomeArq, ios::binary | ios::trunc);
-		
-		if(!arqSaidaBin[i].is_open()){
-			delete[] arqSaidaBinOrdenada;
-			throw runtime_error("Nao foi possivel criar os arquivos de saida ordenados!");
-		}
+		string nomeArq = nomeBaseAqr +  to_string(i) + ".bin";
+		long numRegistro = tamanho_Registro(nomeArq);
+		Registro* umRegistroInterno = new Registro[numRegistro];
 
-		Registro* umRegistro = new Registro[numPartes]; 
-		ifstream arqEntrada()
+	// 2. abrir o arquivo e ler, colocando os dados em binario no umRegistroInterno
+		ifstream arqEntrada(nomeArq, ios::binary);
+		arqEntrada.read(reinterpret_cast<char*>(umRegistroInterno), numRegistro*sizeof(Registro));
+		arqEntrada.close();
+
+	// 3. ordenar o array internamente
+		mergeSortIterativo(umRegistroInterno,numRegistro);
+
+	// 4. jogar os arrays ordenados nesse meu arquivo, mas antes deletar oq estava escrito
+		ofstream arqSaida(nomeArq, ios::binary | ios::trunc); // deleta com o trunc
+		arqSaida.write(reinterpret_cast<char*>(umRegistroInterno), numRegistro * sizeof(Registro));
+
+    // 5. fechar todos os arquivos
+    arqSaida.close();
+
+    // 6. liberar a memória alocada para o array de arquivos
+    delete[] umRegistroInterno;
 	}
 
+	cout << "Ordenacao interna concluida!\n";
 }
 
 
